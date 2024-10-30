99924978431c2e260ef814dc7a27042bd5b41d2c
Thales M <mbthales10@gmail.com>
2024-10-30 20:10:18 -0300

    Adicionei novos modelos 3D e implementei alguns ajustes

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+import "./reset.css";
 import "./style.css";
 
 const oi = () => {
+  console.log("hello");
   const app = document.querySelector("#app");
-  document.getElementById("teste").style.display = "block";
+  document.getElementById("menu").style.display = "block";
   app.style.display = "none";
 };
 
@@ -11,22 +13,36 @@ window.oi = oi;
 const test = () => {
   const btn = document.getElementById("btn-active-ar");
   const app = document.querySelector("#app");
+  const polyhedronProperty = document.getElementById("property-input");
+
+  const teste = {
+    edge: "./assets/models/cubo-arestas.glb",
+    vertex: "./assets/models/cubo-vertices.glb",
+    face: "./assets/models/cubo-faces.glb",
+  };
 
   btn.addEventListener("click", () => {
-    document.getElementById("teste").style.display = "none";
+    document.getElementById("menu").style.display = "none";
 
     app.innerHTML = `
-        <a-scene embedded arjs="trackingMethod: best; debugUIEnabled: false;" vr-mode-ui="enabled: false;">
-          <button onclick="oi()" style="position: absolute">teste</button>
-          <a-marker preset="hiro">
-            <a-entity
-              scale="0.05 0.05 0.05"
-              gltf-model="./assets/models/poliedro.glb"
-            ></a-entity>
-          </a-marker>
-          <a-entity camera></a-entity>
-        </a-scene>
+    <button id="btn-voltar" class="btn-voltar">Voltar</button>
+    <a-scene embedded arjs="trackingMethod: best; debugUIEnabled: false" vr-mode-ui="enabled: false;">
+      <a-marker preset="hiro">
+        <a-entity
+          scale="0.05 0.05 0.05"
+          gltf-model="./assets/models/cubo-arestas.glb"
+        ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
     `;
+
+    const test = document.getElementById("btn-voltar");
+    console.log(test);
+    test.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("hello!");
+    });
   });
 };
 
